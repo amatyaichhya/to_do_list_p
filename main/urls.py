73eab32611c1391130358cad1f5e45129f6a7850
amatyaichhya <amatyaichhya@gmail.com>
2020-08-30 13:42:58 +0545

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from main import views
+from django.urls import path
+
+urlpatterns = [
+  url(r'^$', views.home, name='home'),
+  path('add_todo/', views.add_todo),
+  path('delete_todo/<int:todo_id>/', views.delete_todo),
+]
